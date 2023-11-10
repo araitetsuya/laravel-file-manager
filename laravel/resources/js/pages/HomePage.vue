3f6a2863c850da28365786e9a7baea08f311116f
araitetsuya <arai@valeur3.com>
2023-11-10 19:25:14 +0900

    TailwindCSSのサンプル実装

--- laravel/resources/js/pages/HomePage.vue
+++ laravel/resources/js/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Home</h1>
+  <h1 class="mb-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Home</h1>
   <router-link to="/sample">SamplePageに移動する</router-link>
 </template>
 
--- laravel/resources/js/pages/SamplePage.vue
+++ laravel/resources/js/pages/SamplePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>SamplePage</h1>
+  <h1 class="mb-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">SamplePage</h1>
   <router-link to="/">Homeに戻る</router-link>
 </template>
 
